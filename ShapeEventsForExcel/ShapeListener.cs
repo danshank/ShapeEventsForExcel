cc1b117fc7e20b1006e614d0eed296689d0061b2
Daniel Shank <Dshank@suffolk.com>
2018-02-01 14:29:24 -0500

    Remove a bunch of unused libraries, create the class that's called from VBA, add an event that is called with the shape name everytime

---
+++ ShapeEventsForExcel/ShapeListener.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ShapeEventsForExcel
+{
+    public class ShapeListener
+    {
+        public event EventHandler<string> ShapeClick;
+
+        public void Click(string shapeName)
+        {
+            OnShapeClick(shapeName);
+        }
+
+        protected virtual void OnShapeClick(string shapeName)
+        {
+            ShapeClick?.Invoke(this, shapeName);
+        }
+    }
+}
