8e3c88d6c7cdf2f81237fb7ff4298606cd94d0aa
Daniel Shank <Dshank@suffolk.com>
2018-02-01 15:53:41 -0500

    Add static "global" manager class which gives access to the shape listener / vba connector

---
+++ ShapeEventsForExcel/Manager.cs
@@ -0,0 +1,12 @@
+﻿namespace ShapeEventsForExcel
+{
+    public static class Manager
+    {
+        public static ShapeListener VbaConnector { get; }
+
+        static Manager()
+        {
+            VbaConnector = new ShapeListener();
+        }
+    }
+}
